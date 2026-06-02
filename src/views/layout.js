c27e413a920f9dc78c6fe89081c4cabc95a857aa
Brandon-Valdelomar <brvaldelomarco@est.utn.ac.cr>
2026-06-02 10:15:14 -0600

    Tercer commit con todas las carpetas y archivos

---
+++ src/views/layout.js
@@ -0,0 +1,30 @@
+export function layout(titulo, contenido) { return ` <!DOCTYPE html>
+     <html lang="es">
+      <head> 
+      <meta charset="UTF-8"> 
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <title>${titulo}
+       </title> <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" >
+        </head> 
+        <body class="bg-light">
+         <nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-4"> 
+         <div class="container">
+          <a class="navbar-brand" href="/">
+          Plataforma de Tareas</a>
+
+           <div class="navbar-nav"> 
+          <a class="nav-link" href="/">Inicio</a>
+          <a class="nav-link" href="/tareas">Tareas</a>
+          <a class="nav-link" href="/tareas/nueva">Nueva tarea</a> 
+            </div> 
+            </div>
+             </nav>
+
+              <main class="container">
+               ${contenido} 
+               </main> 
+               <script 
+               src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"> 
+               </script>
+                </body>
+                 </html> `; }
